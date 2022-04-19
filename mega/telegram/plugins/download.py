d50b5f945481241983343e9081e50ba1ff0a2d79
DX Botz <77914965+dxbotz@users.noreply.github.com>
2022-04-20 00:45:13 +0530

    Update download.py

--- mega/telegram/plugins/download.py
+++ mega/telegram/plugins/download.py
@@ -6,6 +6,7 @@ import asyncio
 import logging
 import humanfriendly as size
 import humanfriendly as humanbyte
+import urllib.parse
 from sample_config import Config
 from mega.common import Common
 from pyrogram import emoji, Client
@@ -15,6 +16,13 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from translation import Translation
 
 from ..utils import filters
+
+def get_media_file_name(message):
+    media = m.video or m.document or m.audio
+    if media and media.file_name:
+        return urllib.parse.quote_plus(media.file_name)
+    else:
+        return None
 
 
 @Client.on_message(filters.document | filters.video)
@@ -55,8 +63,9 @@ async def download_user(bot, message):
         Config.CHANNEL_ID,
         f"**This File☝️ Sender Name :-** [{message.from_user.first_name}](tg://user?id={message.chat.id})"
     )
+    file_name = get_media_file_name(message)
     if message.video is not None:
-        file_name = message.video.file_name
+        file_name_ = message.video.file_name
         file_size = message.video.file_size
         file_size = humanbyte.format_size(message.video.file_size, binary=True)
     elif message.document is not None:
@@ -64,11 +73,11 @@ async def download_user(bot, message):
         file_size = message.document.file_size
         file_size = humanbyte.format_size(message.document.file_size, binary=True)
 
-    file_link = f"https://{Common().web_fqdn}/DX_Bots/{fd_msg.message_id}" if Common().on_heroku else \
+    file_link = f"https://{Common().web_fqdn}/MalluMovies/{fd_msg.message_id}/{file_name}" if Common().on_heroku else \
         f"http://{Common().web_fqdn}:{Common().web_port}/{fd_msg.message_id}"
 
     await first.edit(
-        text=Translation.LINK_TEXT + Translation.FILE_NAME.format(file_name) + Translation.FILE_SIZE.format(file_size) + Translation.FILE_LINK.format(file_link),
+        text=Translation.LINK_TEXT + Translation.FILE_NAME.format(file_name_) + Translation.FILE_SIZE.format(file_size) + Translation.FILE_LINK.format(file_link),
         reply_markup=InlineKeyboardMarkup(
             [
                 [InlineKeyboardButton(text=f"📩 Download Link", url=file_link)],
