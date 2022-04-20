b22d6e556f66a8503df882c564cdb5541d112c29
DX Botz <77914965+dxbotz@users.noreply.github.com>
2022-04-20 13:49:46 +0530

    Update common.py

--- mega/telegram/plugins/common.py
+++ mega/telegram/plugins/common.py
@@ -10,42 +10,49 @@ from translation import Translation
 from ..utils import filters
 
 
-@Client.on_message(filters.command(["start"]))
+@Client.on_message(filters.private & filters.command(["start"]))
 async def start(bot, update):
+    btn = [[
+        InlineKeyboardButton('Help', callback_data='help_btn'),
+        InlineKeyboardButton('About', callback_data='about_btn'),
+        InlineKeyboardButton('Close', callback_data='close_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(btn)
     await update.reply_text(
-        text="Hai im file to link bot")
+        text=Translation.START_TEXT.format(update.from_user.first_name),
+        parse_mode="html",
+        reply_markup=reply_markup,
+        disable_web_page_preview=True)
 
-@Client.on_message(filters.command(["help"]))
+@Client.on_message(filters.private & filters.command(["help"]))
 async def help_user(bot, update):
+    btn = [[
+        InlineKeyboardButton('Home', callback_data='home_btn'),
+        InlineKeyboardButton('About', callback_data='about_btn'),
+        InlineKeyboardButton('Close', callback_data='close_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(btn)
     await bot.send_message(
         chat_id=update.chat.id,
         text=Translation.HELP_TEXT.format(
                 update.from_user.first_name),
-        parse_mode="html")
-
-@Client.on_callback_query(filters.regex(r'^help_btn$'))
-async def help_button(bot, update):
-    await update.answer()
-    buttons = [[
-        InlineKeyboardButton('📌 Support Group', url='https://t.me/Dx_Support'),
-        InlineKeyboardButton('Update Channel 📜', url='https://t.me/DX_Botz')
-        ],[
-        InlineKeyboardButton('♻️Share', url='tg://msg?text=**Hey%20Broh**%F0%9F%A5%B0%2C%0A__This%20Bot%20Generate%20Instant%20File%20Direct%20Download%20Link__%F0%9F%94%A5%0A%0A**Bot%20Link**%20%3A-%20%40FileToLinkDXBot'),
-        InlineKeyboardButton('Close 🔐', callback_data='cancel_btn')
-    ]]
-    reply_markup = InlineKeyboardMarkup(buttons)
-    await update.edit_message_text(
-        text=Translation.HELP_TEXT.format(
-                update.from_user.first_name),
+        parse_mode="html",
         reply_markup=reply_markup,
-        parse_mode="html")
+        disable_web_page_preview=True)
 
-@Client.on_callback_query(filters.regex(r'^cancel_btn$'))
-async def cancel(bot, update):
-    """Cancel and delete message"""
-    await update.answer()
-    #await message.delete()
-    await update.message.delete()
+@Client.on_message(filters.private & filters.command(["about"]))
+async def about_user(bot, update):
+    btn = [[
+        InlineKeyboardButton('Home', callback_data='home_btn'),
+        InlineKeyboardButton('Help', callback_data='help_btn'),
+        InlineKeyboardButton('Close', callback_data='close_btn')
+    ]]
+    reply_markup = InlineKeyboardMarkup(btn)
+    await update.reply_text(
+        text=Translation.ABOUT_TEXT,
+        reply_markup=reply_markup,
+        parse_mode="html",
+        disable_web_page_preview=True)
 
 @Client.on_message(group=-1)
 async def stop_user_from_doing_anything(_, message: Message):
