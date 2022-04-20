6545ffc68ae9bd97b1e5d9cd48682d65045247d0
DX Botz <77914965+dxbotz@users.noreply.github.com>
2022-04-20 20:59:23 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -3,3 +3,5 @@ import os
 class Config(object):
     # add config vars for database
     DB_URI = os.environ.get("DATABASE_URL", "")
+    OWNER = [int(i) for i in os.environ.get("OWNER", "809546777").split(" ")]  
+
