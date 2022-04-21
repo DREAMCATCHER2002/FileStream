52af13e47acab85b16853a80e7f2ae1588633899
DX Botz <77914965+dxbotz@users.noreply.github.com>
2022-04-21 11:17:59 +0530

    Update common.py

--- mega/telegram/plugins/common.py
+++ mega/telegram/plugins/common.py
@@ -61,6 +61,44 @@ async def about_user(bot, update):
         parse_mode="html",
         disable_web_page_preview=True)
 
+@Client.on_message(filters.private & filters.command(["ban_user"]))
+async def ban(c, m):
+    
+    if len(m.command) == 1:
+        await m.reply_text(
+            f"Use this command to ban any user from the bot.\n\nUsage:\n\n`/ban_user user_id ban_duration ban_reason`\n\nEg: `/ban_user 1234567 28 You misused me.`\n This will ban user with id `1234567` for `28` days for the reason `You misused me`.",
+            quote=True
+        )
+        return
+    
+    try:
+        user_id = int(m.command[1])
+        ban_duration = int(m.command[2])
+        ban_reason = ' '.join(m.command[3:])
+        ban_log_text = f"Banning user {user_id} for {ban_duration} days for the reason {ban_reason}."
+        
+        try:
+            await c.send_message(
+                user_id,
+                f"You are banned to use this bot for **{ban_duration}** day(s) for the reason __{ban_reason}__ \n\n**Message from the admin**"
+            )
+            ban_log_text += '\n\nUser notified successfully!'
+        except:
+            traceback.print_exc()
+            ban_log_text += f"\n\nUser notification failed! \n\n`{traceback.format_exc()}`"
+        await ban_user(user_id, ban_duration, ban_reason)
+        print(ban_log_text)
+        await m.reply_text(
+            ban_log_text,
+            quote=True
+        )
+    except:
+        traceback.print_exc()
+        await m.reply_text(
+            f"Error occoured! Traceback given below\n\n`{traceback.format_exc()}`",
+            quote=True
+        )
+
 #@Client.on_message(group=-1)
 #async def stop_user_from_doing_anything(_, message: Message):
 #    allowed_users = Common().allowed_users
