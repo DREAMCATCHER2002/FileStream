e965e5d5f6c6c00acc6365d28b2a29d294ba596b
DX Botz <77914965+dxbotz@users.noreply.github.com>
2022-04-21 17:26:23 +0530

    Update common.py

--- mega/telegram/plugins/common.py
+++ mega/telegram/plugins/common.py
@@ -8,7 +8,7 @@ from pyrogram.types import Message, InlineKeyboardMarkup, InlineKeyboardButton, 
 from ...telegram import Common
 from translation import Translation
 from ..utils import filters
-from mega.database.database import is_user_exist, add_user, ban_user, remove_ban, get_all_banned_users
+from mega.database.database import is_user_exist, add_user, ban_user, remove_ban, get_all_banned_users, get_ban_status
 
 
 @Client.on_message(filters.private & filters.command(["start"]))
