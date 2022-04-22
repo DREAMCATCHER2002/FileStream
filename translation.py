7aede837baf4d9399b76faf4087d91445ccd250e
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-04-22 22:13:57 +0300

    Update translation.py

--- translation.py
+++ translation.py
@@ -1,5 +1,5 @@
 class Translation(object):
-    LINK_TEXT = """<i><b>🔗 Yᴏᴜʀ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇᴅ 😜</b></i>
+    LINK_TEXT = """<u><i><b>🔗 Yᴏᴜʀ Dᴏᴡɴʟᴏᴀᴅ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴇᴅ 😜</b></i></u>
 
 <b>📂 Fɪʟᴇ Nᴀᴍᴇ :</b> <code>{}</code>
 
@@ -9,31 +9,27 @@ class Translation(object):
 
 🚀 <b>Cᴏᴘʏ ᴀɴᴅ Pᴀꜱᴛᴇ Tʜɪꜱ Lɪɴᴋ ɪɴ Yᴏᴜʀ Bʀᴏᴡꜱᴇʀ ᴀɴᴅ ᴛʜᴇ Fɪʟᴇ Dᴏᴡɴʟᴏᴀᴅ Wɪʟʟ Sᴛᴀʀᴛ Iᴍᴍᴇᴅɪᴀᴛᴇʟʏ!!</b>
 
-🚸 <b>Nᴏᴛᴇ : Tʜɪs Pᴇʀᴍᴀɴᴇɴᴛ Lɪɴᴋ, Nᴏᴛ Exᴘɪʀᴇᴅ</b>
-
-📽️ <b>Rᴇϙᴜᴇsᴛ Mᴏᴠɪᴇs : @FileSearch1Bot</b>
-
 """
     START_TEXT = """
-<i><b>Hᴇʏ 👋 {} 😍</b></i>\n
-<i><b>I'ᴍ Sɪᴍᴘʟᴇ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ ᴛᴏ Iɴꜱᴛᴀɴᴛ Pᴇʀᴍᴇɴᴀɴᴛ Dɪʀᴇᴄᴛ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ 😜</b></i>\n
-<i><u>🔹 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u></i>\n
+<b>Hᴇʏ 👋 {} 😍</b>\n
+<b>I'ᴍ Sɪᴍᴘʟᴇ Tᴇʟᴇɢʀᴀᴍ Fɪʟᴇ ᴛᴏ Iɴꜱᴛᴀɴᴛ Pᴇʀᴍᴇɴᴀɴᴛ Dɪʀᴇᴄᴛ Lɪɴᴋ Gᴇɴᴇʀᴀᴛᴏʀ Bᴏᴛ 😜</b>\n
+<b><u>🔹 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u></b>\n
 <b>🔞 Pᴏʀɴ Cᴏɴᴛᴇɴᴛꜱ Lᴇᴀᴅꜱ ᴛᴏ Pᴇʀᴍᴀɴᴇɴᴛ Bᴀɴ Yᴏᴜ ❌</b>\n
 <b>🤖 Bᴏᴛ Wɪʟʟ Wᴏʀᴋ 24x7</b>\n
 <b>🍃 Rᴇϙᴜᴇsᴛ Mᴏᴠɪᴇs : @FileSearch1Bot</b>"""
 
     HELP_TEXT = """
-<i>🔻 Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ.</i>
-<i>🔻 I ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴇxᴛᴇʀɴᴀʟ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ !.</i>
-<i>🔻 Tʜɪs Pᴇʀᴍᴇᴀɴᴛ Lɪɴᴋ Wɪᴛʜ Fᴀsᴛᴇsᴛ Sᴘᴇᴇᴅ</i>
-<i>🔻 Bᴏᴛ Nᴏᴛ Sᴜᴘᴘᴏʀᴛ Cʜᴀɴɴᴇʟ Oʀ Gʀᴏᴜᴘ !</i>
-<i>🔻 Bᴏᴛ Wɪʟʟ Wᴏʀᴋ 24x7 !</i>\n
-<i><u>🔸 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u></i>\n
+<b>🔻 Sᴇɴᴅ ᴍᴇ ᴀɴʏ ꜰɪʟᴇ (ᴏʀ) ᴍᴇᴅɪᴀ ꜰʀᴏᴍ ᴛᴇʟᴇɢʀᴀᴍ.</b>
+<b>🔻 I ᴡɪʟʟ ᴘʀᴏᴠɪᴅᴇ ᴇxᴛᴇʀɴᴀʟ ᴅɪʀᴇᴄᴛ ᴅᴏᴡɴʟᴏᴀᴅ ʟɪɴᴋ !.</b>
+<b>🔻 Tʜɪs Pᴇʀᴍᴇᴀɴᴛ Lɪɴᴋ Wɪᴛʜ Fᴀsᴛᴇsᴛ Sᴘᴇᴇᴅ</b>
+<b>🔻 Bᴏᴛ Nᴏᴛ Sᴜᴘᴘᴏʀᴛ Cʜᴀɴɴᴇʟ Oʀ Gʀᴏᴜᴘ !</b>
+<b>🔻 Bᴏᴛ Wɪʟʟ Wᴏʀᴋ 24x7 !</b>\n
+<b><u>🔸 𝗪𝗔𝗥𝗡𝗜𝗡𝗚 🚸</u></b>\n
 <b>🔞 Pᴏʀɴ Cᴏɴᴛᴇɴᴛꜱ Lᴇᴀᴅꜱ ᴛᴏ Pᴇʀᴍᴀɴᴇɴᴛ Bᴀɴ Yᴏᴜ ❌</b>\n
-<i>Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ (Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ</i> <b>: <a href='https://t.me/OTTHelpbot'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>"""
+<b>Cᴏɴᴛᴀᴄᴛ Dᴇᴠᴇʟᴏᴘᴇʀ (Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ</b> <b>: <a href='https://t.me/OTTHelpbot'>[ ᴄʟɪᴄᴋ ʜᴇʀᴇ ]</a></b>"""
 
     ABOUT_TEXT = """
-<b>⚜ Mʏ Nᴀᴍᴇ : 📂 TG File To Link Bot Pro 🔗</b>\n
+<b>⚜ Mʏ Nᴀᴍᴇ : TG File To Link Bot Pro</b>\n
 <b>🔸Bᴜɪʟᴅ Sᴛᴀᴛᴜs : <a href='https://telegram.me/ottupdatetg'>3.0.1 [BETA]</a></b>\n
 <b>🔹Sᴏᴜʀᴄᴇ : <a href='https://t.me/ottupdatetg'>Cʟɪᴄᴋ Hᴇʀᴇ</a></b>\n
 <b>🔸Cʀᴇᴅɪᴛs : <a href='https://t.me/ottupdatetg'>Eᴠᴇʀʏᴏɴᴇ ɪɴ ᴛʜɪs Jᴏᴜʀɴᴇʏ</a></b>\n
@@ -43,5 +39,5 @@ class Translation(object):
 
     JOIN = """<b>Hai 👋 {}</b> 😍
 
-<b>Sorry Dear Please Subscribe my Project channel then use again me 🤭</b>"""
+<b>Sorry Dear ! Please Join My Project Channel First To Use Me 🤭</b>"""
 
